1235c8226889fdc3419bf5e08c350838a00979a7
bukinoshita <bukinoshita@gmail.com>
2020-04-22 14:31:00 -0300

    fix: lint

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// Packages
 const { parse } = require('url')
 const qs = require('querystring')
 const { send } = require('micro')
 const alex = require('alex')
 
-const handleQuery = req => {
-  const url = parse(req.url)
+const handleQuery = (request) => {
+  const url = parse(request.url)
   const query = qs.parse(url.query)
 
   if (query && query.text) {
@@ -15,12 +14,12 @@ const handleQuery = req => {
   throw new TypeError('Text is required')
 }
 
-module.exports = async (req, res) => {
+module.exports = async (request, result) => {
   try {
-    const { text } = handleQuery(req)
+    const { text } = handleQuery(request)
     const data = alex(text).messages
-    return send(res, 200, { data })
+    return send(result, 200, { data })
   } catch (error) {
-    return send(res, 400, { error: { message: error.message } })
+    return send(result, 400, { error: { message: error.message } })
   }
 }
