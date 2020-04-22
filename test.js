6077b9a4760ad370a5af0262ad54b9a2965f32cd
bukinoshita <bukinoshita@gmail.com>
2020-04-22 14:26:11 -0300

    fix: lint

--- test.js
+++ test.js
@@ -7,15 +7,15 @@ const api = require('.')
 const service = request(api)
 
 test('should fail if no URL is defined', async t => {
-  const res = await service.get('/').expect(400)
+  const result = await service.get('/').expect(400)
 
-  t.is(res.status, 400)
+  t.is(result.status, 400)
 })
 
 test('should succeed with text schema defined', async t => {
   const text = 'The slaves were read-only copies of master. But not to worry, he was a cripple.'
-  const res = await service.get(`/?text=${text}`).expect(200)
+  const result = await service.get(`/?text=${text}`).expect(200)
 
-  t.is(res.status, 200)
-  t.deepEqual(res.body.data.length, 4)
+  t.is(result.status, 200)
+  t.is(result.body.data.length, 4)
 })
